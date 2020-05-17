2f2f5622d04e10df5da3b35d47c31eaf2fe8ee84
Dyas Dendi Andika <52526901+dydean32@users.noreply.github.com>
2020-05-17 12:59:34 +0700

    Create telnetlib2.py

---
+++ telnetlib2.py
@@ -0,0 +1,40 @@
+#koneksi ke multiple switch (config ip loopback)
+import telnetlib
+import getpass
+
+user = input("Masukan username router : ")
+password = getpass.getpass("Masukan Password : ")
+
+host = ['192.168.1.2','192.168.1.3','192.168.1.4']
+
+for device_list in host:
+    print("Konfigurasi ip {}: ".format(device_list))
+    tn = telnetlib.Telnet(device_list)
+
+    tn.read_until(b"Username: ")
+    tn.write(user.encode('ascii') + b"\n")
+
+    if password:
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b"\n")
+
+    tn.write(b"enable\n")
+
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+    tn.write(b"conf t\n")
+    tn.write(b"int lo 100\n")
+    if device_list == '192.168.1.2':
+        ip = "ip add 1.1.1.1 255.255.255.255"
+        tn.write(ip.encode('ascii') + b"\n")
+    elif device_list == '192.168.1.3':
+        ip = "ip add 2.2.2.2 255.255.255.255"
+        tn.write(ip.encode('ascii') + b"\n")
+    elif device_list == '192.168.1.4':
+        ip = "ip add 3.3.3.3 255.255.255.255"
+        tn.write(ip.encode('ascii') + b"\n")
+    tn.write(b"no sh\n")
+    tn.write(b"end\n")
+    tn.write(b"exit\n")
+    print(tn.read_all().decode('ascii'))
