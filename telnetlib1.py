5254785884e67a03615fc12857c1d3c593272db3
Dyas Dendi Andika <52526901+dydean32@users.noreply.github.com>
2020-05-17 12:58:15 +0700

    Create telnetlib1.py

---
+++ telnetlib1.py
@@ -0,0 +1,27 @@
+# scipt conenct network automation to single switch
+import telnetlib
+import getpass
+
+host = input("Masukan Ip Router : ")
+user = input("Masukan username router : ")
+password = getpass.getpass("Masukan Password : ")
+
+tn = telnetlib.Telnet(host)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")      #untuk melakukan encode nilai pada variabel user.
+                                            #bedanya python3 dengan python2 text harus di encode dulu
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"enable\n")
+
+tn.read_until(b"Password: ")
+tn.write(password.encode('ascii') + b"\n")
+write(b"conf t\n")
+tn.write(b"int vlan 100\n")
+tn.write(b"name vlan100\n")
+tn.write(b"end\n")
+tn.write(b"exit\n")
+print(tn.read_all().decode('ascii')) #untuk menampilkans semua teks dan mencetak konfigurasi
